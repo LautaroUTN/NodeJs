92c643940800f937a8342dc33a29309457721f11
Matelog <lautaro.corfield@gmail.com>
2018-12-15 15:31:53 -0300

    Automatizacion hecha. Estoy trabajando en el refresh del proyecto y tambien en Angular

--- node/app.js
+++ node/app.js
@@ -8,8 +8,13 @@ const port = 8000;//puerto en el que se va a trabajar
 const app = express();//framework express
 //se ejecuta todo app de routes
 routes(app);
+
+//Trabajando en ésto(Para refrescar la página principal)
+//var path = __dirname + 'app.js'
+//app.use(reload(path));
+
 //Verifica y manda a la consola si el servidor prendio correctamente
 const server = app.listen(port, (error) => {
     if (error) return console.log(`Error: ${error}`);
-    console.log(`Server listening on port ${server.address().port}`);
+    console.log(`Servidor funcionando en ${server.address().port}`);
 })
--- node/routes/routes.js
+++ node/routes/routes.js
@@ -4,23 +4,35 @@ Archivo de rutas
 */
 //llamamos a la conexión a la base de datos 
 const pool = require('../data/config');
+//Esta parte del codigo consta del return donde nos muestra un resultado similar al pedido
+//en el ejercicio 1 a. Funciona dirigiendonos al dominio
+//localhost:8000/cotizacion/dolar
+//En vez de dolar puede ser:
+//euro, libra o sol
 const router = app => {
     app.get('/cotizacion/:mon', (request, response) => {
         const mon = request.params.mon; //variable que utilizamos(dolar, euro, libra, real)
         pool.query('SELECT moneda, precio FROM cotizacion WHERE moneda = ?', mon, (error, result) => {
             if (error) throw error;
-            response.send(
-                "<html><head></head><body><div style='width:500px; height:500px;background-color:aqua;'>"+
-                result[0]["moneda"]+" "+result[0]["precio"]+"</div></body></html>"
-            );
+            response.send(result[0]);
         });
-    });
+    });   
+//Esta parte del ejercicio consta del return con diseño donde nos muestra 4 monedas(dolar, euro, real, libra)
      app.get('/cotizacion', (request, response) => {
         pool.query('SELECT * FROM cotizacion', (error, result) => {
             if (error) throw error;
-                "<html><head></head><body><div style='width:500px; height:500px;background-color:aqua;'>"+
-                result[0]["moneda"]+" "+result[0]["precio"]+"</div></body></html>"
-                response.send(result[i]);
+            var size = Object.keys(result).length;//obtiene el largo del array(cantidad de monedas)
+            for(var x = 0; x<size;x++){
+                //La diferencia entre response.write y response.send es que response.send actua como un
+                //return, solo se puede llamar una vez y termina la instancia.
+                //es por eso que en un for utilizamos response.write
+                response.write(
+                    "<html><head></head><body><div style='width:200px; height:200px;background-color:aqua;'>"+
+                    result[x]["moneda"]+" "+result[x]["precio"]+"</div>"
+                    //response.send("El largo es: "+size)
+                );
+            }
+            response.write("</body></html>");
         });
     });
 }
